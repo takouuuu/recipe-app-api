35fec28aef25f08750effcae7d82d6a7aa468dfc
takouuuu <henry8221991@gmail.com>
2023-10-15 15:43:04 +0800

    User App Code Check

--- app/user/views.py
+++ app/user/views.py
@@ -2,7 +2,6 @@
 Views for the user API.
 '''
 from rest_framework import generics, authentication, permissions
-from rest_framework import generics
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
 
