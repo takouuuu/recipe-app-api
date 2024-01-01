4f4b9878e40c4c0e020fb07960f45d103b896e34
takouuuu <henry8221991@gmail.com>
2024-01-01 14:39:15 +0800

    upload image

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -3,7 +3,10 @@
 from rest_framework import (
     viewsets,
     mixins,
+    status,
     )
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
@@ -29,12 +32,29 @@ class RecipeViewSet(viewsets.ModelViewSet):
         '''Return the serializer class for requests'''
         if self.action == 'list':
             return serializers.RecipeSerializer
+        elif self.action == 'upload_image': #custom action need to add on ur own
+            return serializers.RecipeImageSerializer
 
         return self.serializer_class
 
     def perform_create(self, serializer):
         """Create a new recipe."""
         serializer.save(user=self.request.user)
+    
+    @action(methods=['POST'], detail=True, url_path='upload-image') #added custom action, using actions decorator
+    #actions decorator lets you specify the different HTTP methods support by the custom action (POST in this case)
+    #detail =True means only apply to the detail endpoint, here is to each specify id.
+    def upoload_image(self, request, pk=None):
+        """Upload an image to Recipe"""
+
+        recipe = self.get_object()
+        serializer = self.get_serializer(recipe, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class BaseRecipeAttrViewSet(mixins.UpdateModelMixin,
                             mixins.DestroyModelMixin,
@@ -44,7 +64,7 @@ class BaseRecipeAttrViewSet(mixins.UpdateModelMixin,
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
 
-    def get_querysets(self):
+    def get_queryset(self):
         """Filter querset to authenticated user."""
         return self.queryset.filter(user=self.request.user).order_by('-name')
 
